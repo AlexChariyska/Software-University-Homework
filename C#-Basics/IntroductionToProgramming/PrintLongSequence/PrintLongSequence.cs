10dd5bc8c966518fb9b140c5ebf58e502650972a
Alexandra <alex.chariyska@gmail.com>
2015-02-06 17:37:31 +0200

    Homework - course Programming Fundamentals

---
+++ C#-Basics/IntroductionToProgramming/PrintLongSequence/PrintLongSequence.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class PrintLongSequence
+{
+    static void Main()
+    {
+        int a = 0;
+        int b = -1;
+        int count = 0;
+        do
+        {
+            a = a + 2;
+            b = b - 2;
+            count = count + 1;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+        while (count < 500);
+    }
+}
---
+++ C#-Basics/IntroductionToProgramming/SquareRoot/SquareRoot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class SquareRoot
+{
+    static void Main()
+    {
+        int num = 12345;
+        double result = Math.Sqrt(12345);
+        Console.WriteLine("The square root of 12345 is: {0}.", result);
+    }
+}
+
